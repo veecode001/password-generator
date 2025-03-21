8aa0921cc205791e9b527ff03799043e75d0f717
veecode001 <obiageri04@gmail.com>
2025-03-21 11:53:14 +0100

    removed comment

--- index.py
+++ index.py
@@ -9,18 +9,6 @@ num_letters = int(input("How many letters would you like in your password?\n"))
 num_symbols = int(input(f"How many symbols would you like?\n"))
 num_numbers = int(input(f"How many numbers would you like?\n"))
 
-# password = ""
-# for letter in range(0, num_letters):
-#     password += random.choice(letters)
-#
-# for symbol in range(0, num_symbols):
-#     password += random.choice(symbols)
-#
-# for number in range(0, num_numbers):
-#     password += random.choice(numbers)
-#
-# print(password)
-
 
 
 print("Welcome to the Hard level Pypassword Generator!")
